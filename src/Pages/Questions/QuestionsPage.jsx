0b57d48a4e8da665dd117f3887cae9dbc1e90c8a
Mahendra Balu Pawar <mahendrapawar444666@gmail.com>
2022-04-01 14:23:59 +0530

    Pages and Components added

---
+++ src/Pages/Questions/QuestionsPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../../Components/Navbar/Navbar'
+import Questions from '../../Components/Questions/Questions'
+
+const QuestionsPage = () => {
+  return (
+    <>
+        <Navbar/>
+        <Questions/>
+    </>
+  )
+}
+
+export default QuestionsPage
---
+++ src/Pages/Rule/RulePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../../Components/Navbar/Navbar'
+import Rule from '../../Components/Rule/Rule'
+
+const RulePage = () => {
+  return (
+    <>
+        <Navbar/>
+        <Rule/>
+    </>
+  )
+}
+
+export default RulePage
