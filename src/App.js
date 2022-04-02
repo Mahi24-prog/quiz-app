85448affd398f018587427bc556963b2ac879f35
Mahendra Balu Pawar <mahendrapawar444666@gmail.com>
2022-04-02 10:11:19 +0530

    Routing Added to quiz app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Home from './Pages/Home/Home';
+import Router from './Routing/Router';
 
 function App() {
   return (
     <>
-      <Home/>
+      <Router/>
     </>
   );
 }
